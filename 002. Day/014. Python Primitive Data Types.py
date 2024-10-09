9efe3f539976c6aef79b760278fe0375b6d31f33
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-09 16:43:03 +0200

    002. Day 014. Task v2.04 Progress

--- 002. Day/014. Python Primitive Data Types.py
+++ 002. Day/014. Python Primitive Data Types.py
@@ -18,12 +18,22 @@ ln_wrd = len(wrd)
 
 print(f"Word: {wrd}")
 print(f"\tWord length:{ln_wrd}")
+print()
+
+for idx in range(ln_wrd):
+    print(f"\t{idx}.: {wrd[idx]}")
 
 
 """__Output__"""
 """
 Word: Hello
 	Word length:5
+
+	0.: H
+	1.: e
+	2.: l
+	3.: l
+	4.: o
 
 Process finished with exit code 0
 """
