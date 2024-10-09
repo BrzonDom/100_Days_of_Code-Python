03a64a4d9a7b4c6bb0820395428a0486e68c3198
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-09 16:42:08 +0200

    002. Day 014. Task v2.03 Comment

--- 002. Day/014. Python Primitive Data Types.py
+++ 002. Day/014. Python Primitive Data Types.py
@@ -11,8 +11,19 @@ Data Types
 """
 
 
+"""__Code__"""
+
 wrd = "Hello"
 ln_wrd = len(wrd)
 
 print(f"Word: {wrd}")
 print(f"\tWord length:{ln_wrd}")
+
+
+"""__Output__"""
+"""
+Word: Hello
+	Word length:5
+
+Process finished with exit code 0
+"""
