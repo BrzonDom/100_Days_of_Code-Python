efeab9f873a760abfd2e658d5dc34be756d12057
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-17 21:51:49 +0200

    002. Day 018. Task v2.00 Progress

--- 002. Day/018. 002.Project - Tip Calculator.py
+++ 002. Day/018. 002.Project - Tip Calculator.py
@@ -21,6 +21,11 @@
 
 
 print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
+
+# bill = float(input("What was the total bill? $"))
+# tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+# people = int(input("How many people to split the bill? "))
+
+bill = 150
+tip = 12
+people = 5
