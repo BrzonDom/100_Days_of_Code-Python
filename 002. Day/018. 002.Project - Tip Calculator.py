71940c73d39abe202f0ea048c498704375cb04d6
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-17 21:53:48 +0200

    002. Day 018. Task v2.01 Print Progress

--- 002. Day/018. 002.Project - Tip Calculator.py
+++ 002. Day/018. 002.Project - Tip Calculator.py
@@ -29,3 +29,10 @@ print("Welcome to the tip calculator!")
 bill = 150
 tip = 12
 people = 5
+
+print("What was the total bill? $")
+print(bill)
+print("What percentage tip would you like to give? 10 12 15 ")
+print(tip)
+print("How many people to split the bill? ")
+print(people)
