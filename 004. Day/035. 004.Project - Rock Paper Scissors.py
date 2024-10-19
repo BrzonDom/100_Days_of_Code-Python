636743d1f7b417c9f1e7d11b4ec47b2595a07f93
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-19 19:45:33 +0200

    004.Day 035.Task v1.01 Comment

--- 004. Day/035. 004.Project - Rock Paper Scissors.py
+++ 004. Day/035. 004.Project - Rock Paper Scissors.py
@@ -1,3 +1,12 @@
+"""
+4. Project - Rock Paper Scissors
+
+You are going to build a Rock, Paper, Scissors game. You will need to use what you have learnt about randomisation and Lists to achieve this.
+
+https://appbrewery.github.io/python-day4-demo/
+
+"""
+
 rock = '''
     _______
 ---'   ____)
