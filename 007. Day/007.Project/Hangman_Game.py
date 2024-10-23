8c294d4be306e0112f5793c02cd941a19cdcc546
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-23 21:21:56 +0200

    007.Day Project v2.09 Selection

--- 007. Day/007.Project/Hangman_Game.py
+++ 007. Day/007.Project/Hangman_Game.py
@@ -1,8 +1,5 @@
 import random
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
-
 word_list = ["aardvark", "baboon", "camel"]
 
 word = random.choice(word_list)
