323a87484a6d49f74b988e6c6e0bb3ce372a4683
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-23 21:28:49 +0200

    007.Day Project v2.10 Comment

--- 007. Day/007.Project/Hangman_Game.py
+++ 007. Day/007.Project/Hangman_Game.py
@@ -1,5 +1,9 @@
 import random
 
+# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+
+# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+
 word_list = ["aardvark", "baboon", "camel"]
 
 word = random.choice(word_list)
