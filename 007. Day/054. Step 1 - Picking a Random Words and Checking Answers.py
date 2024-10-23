f880b51b09a86dce15797fd86e252ac5659b0674
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-23 18:06:25 +0200

    007.Day 054.Task v2.05 Selection

--- 007. Day/054. Step 1 - Picking a Random Words and Checking Answers.py
+++ 007. Day/054. Step 1 - Picking a Random Words and Checking Answers.py
@@ -8,8 +8,6 @@ print(f"Word list: {word_list}")
 print(f"Chosen word: {chosen_word}")
 print()
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
 guess = input("Guess a letter: ").lower()
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
