045c3d98298ab9dae45ed08c3f0f7f98a9c27fe4
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-23 21:17:06 +0200

    007.Day Project v2.06 Selection

--- 007. Day/007.Project/Hangman_Game.py
+++ 007. Day/007.Project/Hangman_Game.py
@@ -1,7 +1,5 @@
 import random
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
 
