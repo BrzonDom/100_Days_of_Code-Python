b6a61728511ba9d0836105683bcbb172452aca32
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-23 13:18:57 +0200

    005.Day 041.Task v2.01 Print Progress

--- 005. Day/041. 005.Project - Create a Password Generator.py
+++ 005. Day/041. 005.Project - Create a Password Generator.py
@@ -61,5 +61,11 @@ print("Welcome to the PyPassword Generator!")
 # nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 nr_letters = 6
+print("How many letters would you like in your password?")
+print(nr_letters)
 nr_symbols = 2
+print("How many symbols would you like?")
+print(nr_symbols)
 nr_numbers = 4
+print("How many numbers would you like?")
+print(nr_numbers)
