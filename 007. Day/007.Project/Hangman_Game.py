9d5904abae12cdb0b9b7f4f56ccf6c2c47d0307c
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-24 19:08:33 +0200

    007.Day Project v2.18 Selection

--- 007. Day/007.Project/Hangman_Game.py
+++ 007. Day/007.Project/Hangman_Game.py
@@ -1,7 +1,5 @@
 import random
 
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-
 word_list = ["aardvark", "baboon", "camel"]
 
 word = random.choice(word_list)
