a1e6854f904678e681562c4a858d0323f90e00f7
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-24 23:45:48 +0200

    007.Day Project v3.09 Selection

--- 007. Day/007.Project/Hangman_Game.py
+++ 007. Day/007.Project/Hangman_Game.py
@@ -3,8 +3,6 @@ from Hangman_Art import stages, logo
 
 # TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-
 # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
 
 # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
