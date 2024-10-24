c588dd6001b9bda4f82b412a428b1d3f32813e05
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-24 21:42:15 +0200

    007.Day Project v2.25 Comment

--- 007. Day/007.Project/Hangman_Game.py
+++ 007. Day/007.Project/Hangman_Game.py
@@ -1,5 +1,15 @@
 import random
 
+# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+#  Set 'lives' to equal 6.
+
+# TODO-2: - If guess is not a letter in the chosen_word, Then reduce 'lives' by 1.
+#  If lives goes down to 0 then the game should stop and it should print "You lose."
+
+# TODO-3: - print the ASCII art from 'stages'
+#  that corresponds to the current number of 'lives' the user has remaining.
+
+
 word_list = ["aardvark", "baboon", "camel"]
 
 # word = random.choice(word_list)
