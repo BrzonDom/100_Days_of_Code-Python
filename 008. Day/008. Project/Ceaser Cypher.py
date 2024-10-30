9dda95a3a15fcfc0e16eda07e684b1f6c21ce51b
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-30 22:37:46 +0100

    008.Day Project v1.30 Selection

--- 008. Day/008. Project/Ceaser Cypher.py
+++ 008. Day/008. Project/Ceaser Cypher.py
@@ -1,6 +1,3 @@
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
-
 # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
 
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
