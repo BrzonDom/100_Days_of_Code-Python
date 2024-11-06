320eecb1e0080f13a098090b5f625ecba6abb30c
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-11-06 14:47:47 +0100

    009.Day Project v2.00 Progress

--- 009. Day/009. project/The Secret Auction Program.py
+++ 009. Day/009. project/The Secret Auction Program.py
@@ -2,3 +2,7 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+
+name = input()
+price = input()
+bidders = input()
