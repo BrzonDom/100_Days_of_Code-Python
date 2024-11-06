3a2ab32a3e932bd4cc5828a8f9d53f25b8d3c36d
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-11-06 14:56:09 +0100

    009.Day Project v2.02 Print Progress

--- 009. Day/009. project/The Secret Auction Program.py
+++ 009. Day/009. project/The Secret Auction Program.py
@@ -2,6 +2,12 @@
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+
+print("What is your name ?")
 name = input()
+
+print("What is your bid ?")
 price = input()
+
+print("Are there other bidders ?")
 bidders = input()
