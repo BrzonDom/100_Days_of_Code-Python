656509c16ad14b984f844d16f1773767df177e3f
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-11-06 15:39:20 +0100

    009.Day Project v2.07 Progress

--- 009. Day/009. project/The Secret Auction Program.py
+++ 009. Day/009. project/The Secret Auction Program.py
@@ -8,3 +8,10 @@ bidders = input("Other bidders (Y/N): ")
 
 auction = {}
 auction[name] = price
+
+while bidders == 'Y':
+    name = input("Your Name: ")
+    price = int(input("Your Bid:  "))
+    bidders = input("Other bidders (Y/N): ")
+
+    auction[name] = price
