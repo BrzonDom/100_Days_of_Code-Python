2aa5c9f5af9ee9ddeea5cd6b6449d01b46c1db1a
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-11-06 16:01:23 +0100

    009.Day Project v2.11 Print Progress

--- 009. Day/009. project/The Secret Auction Program.py
+++ 009. Day/009. project/The Secret Auction Program.py
@@ -4,6 +4,7 @@
 name = input("Your Name: ")
 bid = int(input("Your Bid:  "))
 bid_more = input("Other bidders (Y/N): ")
+print()
 
 auction = {}
 auction[name] = bid
@@ -12,8 +13,12 @@ while bid_more == 'Y':
     name = input("Your Name: ")
     bid = int(input("Your Bid:  "))
     bid_more = input("Other bidders (Y/N): ")
+    print()
 
     auction[name] = bid
 
 max_name = max(auction, key=lambda nm: auction[nm])
 max_bid = auction[max_name]
+
+print(f"Winner Name: {max_name}")
+print(f"Winner Bid:  {max_bid}")
