0e584053847be90816e63862afff001315d38b06
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-11-10 16:14:47 +0100

    014.Day Project v2.08 Progress

--- Day_014/Project_014/Higher_Lower_Game.py
+++ Day_014/Project_014/Higher_Lower_Game.py
@@ -1,7 +1,7 @@
+import random
 from Higher_Lower_Data import data
 
 categories = tuple(data[0].keys())
-
 # 0. name
 # 1. follower_count
 # 2. description
@@ -12,13 +12,14 @@ for category in categories:
     print(f"  - {category}")
 print()
 
-print("Accounts:")
-print()
+print(random.choice(data))
 
-for account in data:
-    print(f"\tName: {account[categories[0]]}")
-    print()
-    print(f"\t\tFollowers: {account[categories[1]]}k")
-    print(f"\t\tDescription: {account[categories[2]]}")
-    print(f"\t\tCountry: {account[categories[3]]}")
-    print()
+# print("Accounts:")
+# print()
+# for account in data:
+#     print(f"\tName: {account[categories[0]]}")
+#     print()
+#     print(f"\t\tFollowers: {account[categories[1]]}k")
+#     print(f"\t\tDescription: {account[categories[2]]}")
+#     print(f"\t\tCountry: {account[categories[3]]}")
+#     print()
