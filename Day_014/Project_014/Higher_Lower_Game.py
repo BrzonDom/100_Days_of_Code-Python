08c7fd9ac6a9fc82e1da4642dcd5861f37a99410
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-11-10 16:05:16 +0100

    014.Day Project v2.06 Comment

--- Day_014/Project_014/Higher_Lower_Game.py
+++ Day_014/Project_014/Higher_Lower_Game.py
@@ -2,6 +2,11 @@ from Higher_Lower_Data import data
 
 categories = data[0].keys()
 
+# 0. name
+# 1. follower_count
+# 2. description
+# 3. country
+
 print("Categories:")
 for category in categories:
     print(f"  - {category}")
