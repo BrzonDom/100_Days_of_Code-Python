556fc16964ae91fd775f265d8028f2b28c889ac6
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-11-11 17:29:18 +0100

    014.Day Project v3.09 Print Progress

--- Day_014/Project_014/Higher_Lower_Game.py
+++ Day_014/Project_014/Higher_Lower_Game.py
@@ -38,14 +38,16 @@ for account in accounts:
     account_print(account)
 print()
 
+max_len = 0
+
 print("Accounts:")
 print()
 for account in data:
 
-    str_name = f"\tName: {account['name']}"
-    str_followers = f"\t\tFollowers: {account['follower_count']}k"
-    str_description = f"\t\tDescription: {account['description']}"
-    str_country = f"\t\tCountry: {account['country']}"
+    str_name = ' ' * 4 + f"Name: {account['name']}"
+    str_followers = ' ' * 8 + f"Followers: {account['follower_count']}k"
+    str_description = ' ' * 8 + f"Description: {account['description']}"
+    str_country = ' ' * 8 + f"Country: {account['country']}"
 
     print(str_name)
     print()
@@ -53,3 +55,7 @@ for account in data:
     print(str_description)
     print(str_country)
     print()
+
+    max_len = max(max_len, len(str_name), len(str_followers), len(str_description), len(str_country))
+
+print(max_len)
