260280fdc9b1761451c6566d86c4a60d9898984f
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-11-15 23:00:50 +0100

    014.Day Project v3.14 Progress

--- Day_014/Project_014/Higher_Lower_Game.py
+++ Day_014/Project_014/Higher_Lower_Game.py
@@ -53,19 +53,21 @@ print("Accounts:")
 print()
 for account in data:
 
-    str_name = f"Name: {account['name']}"
-    str_followers = f"Followers: {account['follower_count']}k"
-    str_description = f"Description: {account['description']}"
-    str_country = f"Country: {account['country']}"
+    account_print(account)
 
-    print('+' + '-' * max_len + '+')
-    print('|' + ' ' * 3 + str_name+ ' ' * (max_len - 3 - len(str_name)) + '|')
-    print('+' + '-' * max_len + '+')
-    print('|' + ' ' * 7 + str_followers + ' ' * (max_len - 7 - len(str_followers)) + '|')
-    print('|' + ' ' * 7 + str_description + ' ' * (max_len - 7 - len(str_description)) + '|')
-    print('|' + ' ' * 7 + str_country + ' ' * (max_len - 7 - len(str_country)) + '|')
-    print('+' + '-' * max_len + '+')
-    print()
+    # str_name = f"Name: {account['name']}"
+    # str_followers = f"Followers: {account['follower_count']}k"
+    # str_description = f"Description: {account['description']}"
+    # str_country = f"Country: {account['country']}"
+    #
+    # print('+' + '-' * max_len + '+')
+    # print('|' + ' ' * 3 + str_name+ ' ' * (max_len - 3 - len(str_name)) + '|')
+    # print('+' + '-' * max_len + '+')
+    # print('|' + ' ' * 7 + str_followers + ' ' * (max_len - 7 - len(str_followers)) + '|')
+    # print('|' + ' ' * 7 + str_description + ' ' * (max_len - 7 - len(str_description)) + '|')
+    # print('|' + ' ' * 7 + str_country + ' ' * (max_len - 7 - len(str_country)) + '|')
+    # print('+' + '-' * max_len + '+')
+    # print()
 
 #
 #     max_len = max(max_len, len(str_name), len(str_followers), len(str_description), len(str_country))
