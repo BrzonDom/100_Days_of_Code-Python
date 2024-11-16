00b505e41b01bf962bd81bac5e113bf870b81b74
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-11-16 01:54:42 +0100

    014.Day Project v3.24 Function Progress

--- Day_014/Project_014/Higher_Lower_Game.py
+++ Day_014/Project_014/Higher_Lower_Game.py
@@ -41,6 +41,11 @@ def account_compare_print(accounts):
     print(f"\t\t{str_description}")
     print(f"\t\t{str_country}")
 
+    str_name = f"Name: {account_next['name']}"
+    str_followers = f"Followers: {account_next['follower_count']}k"
+    str_description = f"Description: {account_next['description']}"
+    str_country = f"Country: {account_next['country']}"
+
 
 categories = tuple(data[0].keys())
 # 0. name
