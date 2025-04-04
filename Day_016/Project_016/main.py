8b7161a20f0f0004230e262cee69eed5d6078e50
Dominik Brzoň <brzon.l.dominik@gmail.com>
2025-04-04 22:46:48 +0200

    016.Day Project v01.02 Comment

--- Day_016/Project_016/main.py
+++ Day_016/Project_016/main.py
@@ -1,3 +1,9 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# Print a report
+# Check if sufficient resources
+# Process coins
+# Check if transaction success
+# Make coffee
